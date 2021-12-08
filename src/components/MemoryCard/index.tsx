da74c6cdb7ece4f17489b7cad061b85b8ad78d3f
Jakub Karwatka <jakubkarwatka@gmail.com>
2021-12-08 22:35:14 +0100

    Refactor MemoryCard component

--- src/components/MemoryCard/index.tsx
+++ src/components/MemoryCard/index.tsx
@@ -1,33 +1,48 @@
 import React from 'react'
 import Paper from '@mui/material/Paper';
-import { ReactComponent as Image } from './1.svg';
-import { ReactComponent as Back } from './back.svg';
-import { styled } from '@mui/material/styles';
+import { Box, styled } from '@mui/system';
+import { MemoryCardState } from '../../utils/MemoryCardState';
+import { MemoryCardData } from '../../utils/MemoryCardData';
 
-interface Props {
-  revealed: boolean
-  onClick: () => void
+interface MemoryCardProps extends MemoryCardData {
+  cardBack: string
+  onClick: (id: string) => () => void
 }
 
-export const MemoryCard = ({ revealed, onClick }: Props) => {
-  return (
-    <Paper sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: 300,
-      width: 300
-    }}
-      elevation={6}
-      onClick={onClick}
-    >
-      {
-        revealed
-          ? <Image />
-          : <Back />
-      }
-    </Paper >
-  )
-}
+
+const CardImage = styled('img')({
+  maxWidth: ' 100%',
+  height: 'auto'
+})
+
+//TODO: Refactor displaying Card based on state
+//TODO: Move styled components to separate file
+export const MemoryCard = ({ id, state, img, onClick, cardBack }: MemoryCardProps) => (
+  <Box sx={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+    width: 200
+  }}>
+    {
+      state !== MemoryCardState.Hidden && <Paper sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 4,
+        height: 150,
+        width: 150
+      }}
+        elevation={6}
+        onClick={onClick(id)}
+      >
+        {state === MemoryCardState.FaceUp && <CardImage src={img} />}
+        {state === MemoryCardState.FaceDown && <CardImage src={cardBack} />}
+      </Paper >
+    }
+  </Box>
+)
+
 
 
