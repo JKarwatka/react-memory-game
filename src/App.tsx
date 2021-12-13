90cc1c923ae4f6cbe2ea67a2686c2d29c49468ba
Jakub Karwatka <jakubkarwatka@gmail.com>
2021-12-13 18:20:09 +0100

    Add initializing board at the start of the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import { Container, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
-import { Board } from './components/Board';
+import React, { useEffect } from 'react';
+import { MemoryBoard } from './components/Board';
 import CssBaseline from '@mui/material/CssBaseline';
+import { useAppDispatch } from './store/store';
+import { DEFAULT_NUM_OF_PAIRS } from './utils/consts';
+import { initializeGame } from './store/game/slices';
 
 
 //TODO: Move styling to theme or separate file
-const App = () => (
-  <>
-    <CssBaseline />
-    <Box sx={{
-      minHeight: '100vh',
-      backgroundColor: '#14110F',
-      fontFamily: 'PocketMonk'
-    }}>
-      <Container maxWidth='lg'>
-        <Typography sx={{ fontFamily: 'PocketMonk', textAlign: 'center', color: '#ffcd05', fontSize: '150px' }} variant="h4" component="h4">
-          Memory Game
-        </Typography>;
-        <Board />
-      </Container>
-    </Box>
-  </>
-);
+const App = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initializeGame(DEFAULT_NUM_OF_PAIRS));
+  }, [])
+
+  return (
+    <>
+      <CssBaseline />
+      <Box sx={{
+        minHeight: '100vh',
+        backgroundColor: '#14110F',
+        fontFamily: 'PocketMonk'
+      }}>
+        <Container maxWidth='lg'>
+          <Typography sx={{ fontFamily: 'PocketMonk', textAlign: 'center', color: '#ffcd05', fontSize: '150px' }} variant="h4" component="h4">
+            Memory Game
+          </Typography>;
+          <MemoryBoard />
+        </Container>
+      </Box>
+    </>
+  );
+}
 
 
 export default App;
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,18 +2,37 @@ import _ from 'lodash'
 import { v4 as uuidv4 } from 'uuid'
 import { CARD_NAMES } from './consts'
 import { MemoryCardState } from './enums'
+import { Board, CardId, MemoryCardData } from './interfaces'
 
-export const generateBoard = (size: number) => {
-  const images = _.sampleSize(CARD_NAMES, size)
+export const generateBoard = (numOfPairs: number) => {
+  const images = _.sampleSize(CARD_NAMES, numOfPairs)
+  const board: Board = {}
 
-  return new Array(size * 2)
-    .fill(0)
-    .map((_, i) => ({
-      id: uuidv4(),
-      img: images[i % size],
+  const ids = generateIds(numOfPairs * 2)
+
+  ids.forEach((id, i) => {
+    console.log(id)
+    board[id] = {
+      id,
+      img: images[i % numOfPairs],
       cardState: MemoryCardState.FaceDown
-    }))
-    .sort(() => Math.random() - 0.5)
+    }
+  })
+
+  return board
 }
 
 export const getCardImage = (imgName: string) => `${process.env.PUBLIC_URL}/img/${imgName}`
+
+
+export const areCardsMatching = (cards: MemoryCardData[]) =>
+  cards
+    .map(card => card.img)
+    .every((img, _, arr) => img === arr[0])
+
+
+export const generateIds = (num: number) =>
+  Array.from(new Array(num)).map(_ => uuidv4())
+
+export const generateOrder = (orderArr: CardId[]): CardId[] => orderArr.sort(() => Math.random() - 0.5)
+
